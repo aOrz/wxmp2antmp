e0b09ca3d0bb089ab582d010c1e2ad1d8afa34de
王振 <rememberwz@gmail.com>
2017-12-19 11:31:16 +0800

    fix tips error

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var wxmp2antmp = require('./lib/wxmp2antmp');
 
 if (argv.length < 4) {
     console.log('请输入正确的参数，w2a 微信小程序目录 支付宝小程序目录 如： w2a wxmp antmp');
+    process.exit(1);
 }
 var fromPath = path.resolve(argv[2]);
 var toPath = path.resolve(argv[3]);
@@ -30,7 +31,7 @@ function walk (fromPath, toPath) {
 }
 
 if (fs.existsSync(toPath)) {
-    console.log(`${fromPath}文件夹已存在`);
+    console.log(`${toPath}文件夹已存在`);
 } else if (fs.existsSync(fromPath)) {
     fs.mkdirSync(toPath);
     walk(fromPath, toPath);
